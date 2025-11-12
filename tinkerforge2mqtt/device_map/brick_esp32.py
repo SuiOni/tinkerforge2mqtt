04c9b8ec8f17ea45be2e2c0921bcf04666b96ea8
Sakander Zirai <s.zirai@bias-int.com>
2025-11-12 17:31:38 +0100

    Add exception handling decorators to setup_sensors and setup_callbacks methods

--- tinkerforge2mqtt/device_map/brick_esp32.py
+++ tinkerforge2mqtt/device_map/brick_esp32.py
@@ -4,6 +4,7 @@ from tinkerforge.brick_esp32 import BrickESP32
 
 from tinkerforge2mqtt.device_map import register_map_class
 from tinkerforge2mqtt.device_map_utils.base import DeviceMapBase
+from tinkerforge2mqtt.device_map_utils.utils import print_exception_decorator
 
 
 logger = logging.getLogger(__name__)
@@ -18,3 +19,13 @@ class BrickESP32Mapper(DeviceMapBase):
     def __init__(self, *, device: BrickESP32, **kwargs):
         self.device: BrickESP32 = device
         super().__init__(device=device, **kwargs)
+
+    @print_exception_decorator
+    def setup_sensors(self):
+        super().setup_sensors()
+        # Add any ESP32-specific sensors here if needed
+
+    @print_exception_decorator
+    def setup_callbacks(self):
+        super().setup_callbacks()
+        # Add any ESP32-specific callbacks here if needed
